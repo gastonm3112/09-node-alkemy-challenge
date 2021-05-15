a70cc597a6c95925a94bf289710fe548e77e7b73
Gaston Martinez <gastonm3112@gmail.com>
2021-05-15 14:57:27 -0300

    ADDED: Database connection

---
+++ src/loaders/sequelize/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+const config = require('../../config');
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(
+  config.database.name,
+  config.database.username,
+  config.database.password, {
+  host: config.database.host,
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
