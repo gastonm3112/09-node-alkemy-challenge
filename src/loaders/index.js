8de02323bcd8393cc48b01b69ed9a2af5fe311a4
Gaston Martinez <gastonm3112@gmail.com>
2021-05-19 21:09:56 -0300

    ADDED: Movie - contentType - genderType Models

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -7,8 +7,13 @@ module.exports = async () => {
 
     try {
         await sequelize.authenticate();
-        // sequelize.sync({ alter: true });
-        sequelize.sync();
+        require('../models/characters');
+        require('../models/movies');
+        require('../models/contentTypes');
+        require('../models/genderTypes');
+
+        sequelize.sync({ force: true });
+        // sequelize.sync();
         logger.info('DB loaded and connected');
 
         const server = new ExpressServer();
---
+++ src/models/movies.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../loaders/sequelize');
+
+const Movie = sequelize.define('Movie', {
+  // Model attributes are defined here
+  image: {
+    type: DataTypes.STRING(250),
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING(250),
+    allowNull: false,
+    unique: true
+  },
+  creationDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  calification: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+});
+
+
+
+module.exports = Movie;
+
+const Character = require('./characters');
+const ContentType = require('./contentTypes');
+const GenderType = require('./genderTypes');
+
+Movie.belongsToMany(Character, {
+  through: 'characterMovies',
+  as: 'character',
+
+});
+Movie.belongsTo(ContentType, {
+  foreignKey: "contentTypeId",
+  targetKey: "id"
+});
+
+Movie.belongsTo(GenderType, {
+  foreignKey: "genderTypeId",
+  targetKey: "id"
+});
+
+
+
+
